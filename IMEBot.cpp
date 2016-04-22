d6348cd1bf6f57376285246828291f7159fb48f3
Naum Azeredo <naumazeredo@gmail.com>
2016-04-22 14:46:47 -0300

    Updated constants

--- IMEBot.cpp
+++ IMEBot.cpp
@@ -4,7 +4,7 @@
 #include <cmath>
 #include "util.h"
 
-IMEBot::IMEBot() : controly(0.15f, 0.12f), controlx(0.15f, 0.12f), controla(0.1f, 0.1f)
+IMEBot::IMEBot() : controly(0.18f, 0.125f), controlx(0.18f, 0.125f), controla(0.18f, 0.125f)
 {}
 
 IMEBot::~IMEBot()
@@ -25,33 +25,17 @@ void IMEBot::Process()
   float pdx = controlx.pd(refX, t);
   float pda = controla.pd(refA, t);
 
-  // All
-  /*
-  float f = -pda, b = pda;
-  if (fabs(f) > 1.0f || fabs(b) > 1.0f) {
-    float m = max(fabs(f), fabs(b));
-    f /= m;
-    b /= m;
-  }
-
-  thrust = clamp(pdy);
-  sideThrustFront = f;
-  sideThrustBack = b;
-
-  gamestate->Log(to_string(f) + ", " + to_string(b));
-  */
-
   // XY
+  /*
   thrust = clamp(pdy);
   sideThrustFront = clamp(pdx);
   sideThrustBack = clamp(pdx);
+  */
 
   // Angle
-  /*
   thrust = 0;
   sideThrustFront = -clamp(pda);
   sideThrustBack = clamp(pda);
-  */
 
 
   shoot = 0;

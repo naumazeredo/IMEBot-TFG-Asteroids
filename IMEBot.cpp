ed75782c2fdd3a05eba9855ca232e8b7733900b8
Naum Azeredo <naumazeredo@gmail.com>
2016-04-22 17:00:03 -0300

    Potential field done?

--- IMEBot.cpp
+++ IMEBot.cpp
@@ -15,22 +15,46 @@ IMEBot::~IMEBot()
 void IMEBot::Process()
 {
   float t = gamestate->timeStep * gamestate->tick;
-  float alpha = degtorad(myShip->ang);
+  float shipAngle = degtorad(myShip->ang);
   vec2 shipPos { myShip->posx, myShip->posy };
 
   vec2 resForce {0, 0};
 
   for (auto rock : gamestate->rocks) {
-    vec2 pos { rock.second->posx, rock.second->posy };
-    pos = shipPos - pos;
-    if (mag(pos) < 16.0f) {
-      resForce += norm(pos) * (10.0f / squaremag(pos));
+    vec2 deltaPos { rock.second->posx, rock.second->posy };
+    deltaPos = shipPos - deltaPos;
+    if (mag(deltaPos) < 16.0f) {
+      resForce += norm(deltaPos) * (50.0f / squaremag(deltaPos));
+    }
+  }
+
+  for (auto laser : gamestate->lasers) {
+    vec2 dir { laser.second->velx, laser.second->vely };
+
+    vec2 deltaPos { laser.second->posx, laser.second->posy };
+    deltaPos = shipPos - deltaPos;
+
+    float force = squaremag(dir) / squaremag(deltaPos);
+
+    float projmag = mag(deltaPos) * dot(norm(dir), norm(deltaPos));
+    float projx = (deltaPos - norm(dir) * projmag).x;
+    if (projx < 4.0f) {
+      gamestate->Log("steering!");
+      vec2 perp { -dir.y, dir.x };
+      if (dot(perp, deltaPos) < 0)
+        perp = -perp;
+
+      resForce += norm(perp) * force;
     }
   }
 
   vec2 refForce;
-  refForce.x = resForce.x * cos(alpha) - resForce.y * sin(alpha);
-  refForce.y = resForce.x * sin(alpha) + resForce.y * cos(alpha);
+  refForce.x = resForce.x * cos(shipAngle) - resForce.y * sin(shipAngle);
+  refForce.y = resForce.x * sin(shipAngle) + resForce.y * cos(shipAngle);
+
+  //if (mag(refForce) < 0.
+
+  //gamestate->Log(to_string(refForce.x) + " " + to_string(refForce.y));
 
   thrust = refForce.y;
   sideThrustFront = refForce.x / 2;
--- util.h
+++ util.h
@@ -35,6 +35,6 @@ inline float squaremag(const vec2& a) { return (a.x * a.x) + (a.y * a.y); }
 inline vec2 norm(const vec2& a) { return a/mag(a); }
 inline float dot(const vec2& a, const vec2& b) { return (a.x * b.x) + (a.y * b.y); }
 inline vec2 rotate(const vec2& a, const float angle) { return {a.x * cos(angle) - a.y * sin(angle), a.x * sin(angle) + a.y * cos(angle)}; }
-inline vec2 project(const vec2& a, const float angle) { return {a.x * cos(angle), a.y * sin(angle)}; }
+inline vec2 project(const vec2& a, const float angle) { return {mag(a) * sin(angle), mag(a) * cos(angle)}; }
 
 #endif
